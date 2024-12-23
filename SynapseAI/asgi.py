8e55e2112dfc6d488ec3a9b7e641753023cf7219
Gautham C Sudheer <csgautham091003@gmail.com>
2024-12-23 12:14:41 +0530

    Initial Commit

---
+++ SynapseAI/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SynapseAI project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SynapseAI.settings')
+
+application = get_asgi_application()
